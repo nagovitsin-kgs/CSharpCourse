ea68bc84924d9017104a695749a35e58ac8f0638
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2022-11-29 21:40:38 +0700

    homework01_task02

---
+++ Homework01/Task01/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+
+//  4 -> да 
+// -3 -> нет 
+//  7 -> нет 
+
+Console.WriteLine("Start");
+
+int number; 
+
+            Console.Write("Введите число : ");
+ 
+            number = int.Parse(Console.ReadLine());
+ 
+            if (number % 2 == 0)
+ 
+            {
+ 
+                Console.Write("Введенное число является четным");
+ 
+                Console.Read();
+ 
+            }
+ 
+            else
+ 
+            {
+ 
+                Console.Write("Введенное число нечетное");
+ 
+                Console.Read();
+            }
+
+Console.WriteLine("End");

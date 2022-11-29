d4df98c35a24c842b1938973216c7090e760b16c
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2022-11-29 13:12:37 +0700

    sevinar01_task01_end

---
+++ Seminar01/Task_01/Program.cs
@@ -0,0 +1,9 @@
+﻿// 0. Вывести квадрат числа 
+
+Console.WriteLine ("Start"); // начало
+
+int number = 11; // определили целое число
+int result = number * number; // вычисление квадрата числа
+Console.WriteLine ("result"); // вывод результата
+
+Console.WriteLine ("End"); //окончание

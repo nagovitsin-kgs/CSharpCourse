733397352594175e38b8e69738370f4dd84201ca
nagovitsin <nagovitsin-kgs@mail.ru>
2022-12-03 12:11:07 +0300

    CSarpCourse_commit02

---
+++ Homework01/Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Write number 1: ");
+        int K = int.Parse(Console.ReadLine());
+        Console.Write("Write number 2: ");
+        int N = int.Parse(Console.ReadLine());
+        for (int i = K; i <= N; i++)
+        {
+            if (i % 2 == 0)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        Console.ReadKey();
+    }
+}

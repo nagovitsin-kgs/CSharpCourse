6741da59d2e396ed0929e374310854cb6ec94482
nagovitsin <nagovitsin-kgs@mail.ru>
2022-12-06 00:52:44 +0300

    Homework03__Task21

---
+++ Homework03/Task21/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (0,0); B (1,1), -> ~1.4 
+A (2,4); B (0,7) -> ~3.6
+*/
+
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+//int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+//int z2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+//int C = z1 - z2;
+
+//double length = Math.Sqrt(A * A + B * B + C * C);
+double length = Math.Sqrt(A * A + B * B);
+Console.WriteLine($"Длинна отрезка {length}");
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}

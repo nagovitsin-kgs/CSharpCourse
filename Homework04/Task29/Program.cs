57a5c1e7bed6715a4d84bc99e09d98f43ea4afdd
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2022-12-11 21:53:09 +0700

    CSarpCourse_commi13

--- Homework04/Task29/Program.cs
+++ Homework04/Task29/Program.cs
@@ -1,24 +1,38 @@
-﻿/*
-Задача 29: Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
-5 -> [1, 2, 5, 7, 19]
-3 -> [6, 1, 33]
+﻿/*Задача 29: Напишите программу, которая задаёт массив из N элементов (из диапазона [0-14] ) 
+и выводит на экран десятичное представление числа, записанного в СС по основанию 15.
+N: 3 [9, 4, 12] => 2097
+Для проверки можно использовать https://numsys.ru/convert/2097/10/15
 */
+// Создание массива
+  int[] CreateArray(int count)
+  {
+    return new int[count];
+  }
 
-int lenArray = ReadInt("Введите длинну массива: ");
+  // Заполнение числами от 0-14
+  void Fill(int[] array)
+  {
+    for (int i = 0; i < array.Length; i++)
+    {
+      array[i] = Random.Shared.Next(0, 15);
+    }
+  }
 
-int[] randomArray = new int[lenArray];
-for (int i = 0; i < randomArray.Length; i++)
-{
-    randomArray[i] = new Random().Next(1,9);
-    Console.Write(randomArray[i] + " ");
-}
+  // печать массива
+  string Print(int[] array)
+  {
 
+    string result = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+    {
+      if (array[i] == 10) result += $"A";
+      else if (array[i] == 11) result += $"B";
+      else if (array[i] == 12) result += $"C";
+      else if (array[i] == 13) result += $"D";
+      else if (array[i] == 14) result += $"E";
+      else result += $"{array[i]}";
+    }
 
-// Функция ввода
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
- 
+    return result;
+  }
+  

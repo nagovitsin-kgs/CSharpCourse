371ac2d9d05429ec27fcb9847734280744538a89
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2022-12-13 21:56:08 +0700

    CSarpCourse_commit16

--- HelloCode/ClassLibrary/MyLogic.cs
+++ HelloCode/ClassLibrary/MyLogic.cs
@@ -1,5 +1,11 @@
-﻿namespace ClassLibrary;
-public class MyLogic
-{
+﻿namespace ClassLibrary;       // пространство имён, наша папка (библиотека).
+using static System.Console;  // ввели, чтобы сократить команды, не писать систем и консоле.
 
+public class MyLogic          // указали паблик класс и файл MyLogic // где описываем нвшу логику, методы...
+{
+    public void Pause()
+    {
+        WriteLine("Автор я");
+        ReadKey();
+    }
 }

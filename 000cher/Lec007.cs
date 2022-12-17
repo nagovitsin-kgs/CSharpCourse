b470c1031d628a47f208327a169594233ab6d4c5
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2022-12-17 17:50:44 +0700

    CSarpCourse_commit20

---
+++ 000cher/Lec007.cs
@@ -0,0 +1,8 @@
+public class Lec007
+{
+    static public void Task007()  
+    {
+        // логика ввода, чтобы на консоле выв....
+        System.Console.WriteLine(MyLibrary.SumNumberIn(123));  // сам вызов.... метода из нашей библиотеке...
+    }
+}
--- Mylibrary/Program.cs
+++ Mylibrary/Program.cs
@@ -1,82 +1,100 @@
 ﻿public class MyLibrary
 {
-  // Создание массива
-  public static int[] CreateArray(int count)
-  {
-    return new int[count];
-  }
-
-  // Заполнение числами от 0-14
-  public static void Fill(int[] array)
-  {
-    for (int i = 0; i < array.Length; i++)
+    // Создание массива
+    public static int[] CreateArray(int count)
     {
-      array[i] = Random.Shared.Next(0, 15);
-    }
-  }
-
-  // печать массива
-  public static string Print(int[] array)
-  {
-
-    string result = String.Empty;
-    for (int i = 0; i < array.Length; i++)
-    {
-      if (array[i] == 10) result += $"A";
-      else if (array[i] == 11) result += $"B";
-      else if (array[i] == 12) result += $"C";
-      else if (array[i] == 13) result += $"D";
-      else if (array[i] == 14) result += $"E";
-      else result += $"{array[i]}";
+        return new int[count];
     }
 
-    return result;
-  }
-
-  // a^b =  a*a*a*...*a
-  //           b раз
-  public static int Pow(int a, int b)
-  {
-    int result = 1;
-    for (int i = 0; i < b; i++)
+    // Заполнение числами от 0-14
+    public static void Fill(int[] array)
     {
-      result = result * a;
-    }
-    return result;
-  }
-
-  // написать конвертор из 15-ой в 10-ую
-  //  0  1  2   3
-  // [6, 4, 3, 10]
-  //  3  2  1   0
-  // 10 * 15^0 + 3 * 15^1 + 4 * 15^2 + 6 * 15^3
-  
-  public static int ToDec(int[] value)
-  {
-    int result = 0;
-    int count = value.Length;
-
-    for (int i = 0; i < count; i++)
-    {
-      result += value[i] * Pow(15, count - 1 - i);
+        for (int i = 0; i < array.Length; i++)
+        {
+            array[i] = Random.Shared.Next(0, 15);
+        }
     }
 
-    return result;
-  }
-
-  public static int SumNumberIn(int value)
-  {
-    int r = 0;
-
-    while (value != 0)
+    // Печать массива 1 Вариант:
+    public static string Print01(int[] array)
     {
-      r += value % 10;
-      value = value / 10;
-      // value /= 10;
+        string result = string.Empty;
+        for (int i = 0; i < array.Length; i++)
+        {
+            result += $"{array[i]}";
+        }
+        return result;
     }
-    return r;
-  }
 
+    // Печать массива 2 Вариант:
+
+    public static string Print02(int[] array)
+    {
+        return string.Join(" ", array);
+    }
+
+    // Печать массива 3 Вариант:
+
+    public static string Print03(int[] array)
+    {
+        string result = String.Empty;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] == 10) result += $"A";
+            else if (array[i] == 11) result += $"B";
+            else if (array[i] == 12) result += $"C";
+            else if (array[i] == 13) result += $"D";
+            else if (array[i] == 14) result += $"E";
+            else result += $"{array[i]}";
+        }
+
+        return result;
+    }
+
+    // Возведение числа в степень (a^b =  a*a*a*...*a)  b раз
+               
+    public static int Pow(int a, int b)
+    {
+        int result = 1;
+        for (int i = 0; i < b; i++)
+        {
+            result = result * a;
+        }
+        return result;
+    }
+
+    // Написать конвертор из 15-ой в 10-ую
+
+    //  0  1  2   3
+    // [6, 4, 3, 10]
+    //  3  2  1   0
+    // 10 * 15^0 + 3 * 15^1 + 4 * 15^2 + 6 * 15^3
+
+    public static int ToDec(int[] value)
+    {
+        int result = 0;
+        int count = value.Length;
+
+        for (int i = 0; i < count; i++)
+        {
+            result += value[i] * Pow(15, count - 1 - i);
+        }
+        return result;
+    }
+
+    // Написать сумму ЦИФР числа:
+    public static int SumNumberIn(int value)
+    {
+        int r = 0;
+
+        while (value != 0)
+        {
+            r += value % 10;
+            value = value / 10;
+            // value /= 10;
+        }
+        return r;
+    }
 }
 
 // Введите размерность массива от 1 до 19 включительно: 5

4eb6594eb48a9954b7686f6cc92545b1228d3964
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2022-12-19 17:58:18 +0700

    Task43

--- Homework06/Task43/Program.cs
+++ Homework06/Task43/Program.cs
@@ -3,20 +3,26 @@
 // задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+// 1. Ввод пользователем с клавиатуры.
 Console.Write("Введите k1: ");
-var k1 = Convert.ToDouble(Console.ReadLine());
+double k1 = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Введите b1: ");
-var b1 = Convert.ToDouble(Console.ReadLine());
+double b1 = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Введите k2: ");
-var k2 = Convert.ToDouble(Console.ReadLine());
+double k2 = Convert.ToDouble(Console.ReadLine());
+
 Console.Write("Введите b2: ");
-var b2 = Convert.ToDouble(Console.ReadLine());
+double b2 = Convert.ToDouble(Console.ReadLine());
  
- 
-var x = -(b1 - b2) / (k1 - k2);
-var y = k1 * x + b1;
- 
+// 2. Ложим в переменные x,y значения выведенные из уравнения. 
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+// 3. Выводим x,y  
 x = Math.Round(x, 3);
 y = Math.Round(y, 3);
- 
+
+// 4. Вывод на консоль результата.
 Console.WriteLine($"Пересечение в точке: ({x};{y})");

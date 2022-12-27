960b045eaef3bca98950fa8b4d85a6531219b9f5
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2022-12-27 21:09:59 +0700

    Task60

--- Homework08/Task60/Program.cs
+++ Homework08/Task60/Program.cs
@@ -1,44 +1,54 @@
-﻿/* Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+﻿/* Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
 Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 Массив размером 2 x 2 x 2
-66(0,0,0) 25(0, 1, 0)
-34(1, 0, 0) 41(1, 1, 0)
-27(0, 0, 1) 90(0, 1, 1)
-26(1, 0, 1) 55(1, 1, 1)   */
+66(0, 0, 0)  25(0, 1, 0)
+34(1, 0, 0)  41(1, 1, 0)
+27(0, 0, 1)  90(0, 1, 1)
+26(1, 0, 1)  55(1, 1, 1)    */
 
-int deep1 = InputInt("Введите размерность 1: ");
-int deep2 = InputInt("Введите размерность 2: ");
-int deep3 = InputInt("Введите размерность 3: ");
-int countNums = 89;
+Console.Clear();
 
-if (deep1 * deep2 * deep3 > countNums)
+int dimension1 = InputInt("Введите размерность 1: ");
+int dimension2 = InputInt("Введите размерность 2: ");
+int dimension3 = InputInt("Введите размерность 3: ");
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
+
+int countNumbers = 89;
+
+if (dimension1 * dimension2 * dimension3 > countNumbers)
 {
     Console.Write("Массив слишком большой");
     return;
 }
 
-int[,,] resultNums = Create3DMassive(deep1, deep2, deep3);
+int[,,] resultNumbers = Create3DMassive(dimension1, dimension2, dimension3);
 
-for (int i = 0; i < resultNums.GetLength(0); i++)
+for (int i = 0; i < resultNumbers.GetLength(0); i++)
 {
-    for (int j = 0; j < resultNums.GetLength(1); j++)
+    for (int j = 0; j < resultNumbers.GetLength(1); j++)
     {
-        for (int k = 0; k < resultNums.GetLength(2); k++)
+        for (int k = 0; k < resultNumbers.GetLength(2); k++)
         {
-            Console.WriteLine($"[{i},{j},{k}] - {resultNums[i, j, k]}");
+            Console.WriteLine($"[{i},{j},{k}] - {resultNumbers[i, j, k]}");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-
 int[,,] Create3DMassive(int size1, int size2, int size3)
 {
     int[,,] array = new int[size1, size2, size3];
-    int[] values = new int[countNums];
-    int num
-     = 10;
+
+    int[] values = new int[countNumbers];
+
+    int num = 10;
+
     for (int i = 0; i < values.Length; i++)
         values[i] = num
         ++;
@@ -64,11 +74,4 @@ int[,,] Create3DMassive(int size1, int size2, int size3)
         }
     }
     return array;
-}
-
-
-int InputInt(string output)
-{
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
 }

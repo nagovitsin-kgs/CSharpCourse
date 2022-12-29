a8600e18515132fde91f01b74d08e4b4e3459002
nagovitsin-kgs <118985238+nagovitsin-kgs@users.noreply.github.com>
2022-12-29 22:45:21 +0700

    Task64

--- Homework09/Task64/Program.cs
+++ Homework09/Task64/Program.cs
@@ -1,39 +1,49 @@
 ﻿/* Задача 64: Задайте значение N. 
 Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
-Выполнить с помощью рекурсии.
+Выполнить с помощью рекурсии!
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"   */
 
+//      Декомпозиция:
+// 1. Метод ввода натурального числа.
+// 2. Метод расчёта натуральных чисел в промежутке от N до 1.
+// 3. Метод всех методов и печати на экран.
+
 Console.Clear();
 
-int n = InputInt("Введите положительное число: n = ");
-
-int InputInt(string output)
+// 1. Метод ввода натурального числа от N до 1.
+int InputNaturalNumber(out int n)
 {
-    Console.Write(output);
-    return int.Parse(Console.ReadLine());
+    Console.Write("Введите натуральное число: n = ");
+    n = Convert.ToInt32(Console.ReadLine());
+    return n;
 }
 
-int m = 1;
-int NaturalNumber(int n, int m)
+// 2. Метод расчёта натуральных чисел в промежутке от N до 1.
+int NaturalNumbersToBetweenRecursion(int n, int m)
 {
     while (n < 1)
     {
-        {
-            Console.WriteLine("Вы ввели не положительное число, попробуйте запустить программу ещё раз и ввести верное число! ");
-            return int.Parse(Console.ReadLine());
-        }
+        Console.WriteLine("Это не натуральное число. Натуральные числа - это числа, которые мы употребляем при счёте. Они только положительные. Попробуйте запустить программу ещё раз и ввести верное число! ");
+        return int.Parse(Console.ReadLine());
     }
     if (n == m)
     {
         return n;
     }
-    else
     {
-        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+        Console.Write($"{NaturalNumbersToBetweenRecursion(n, m + 1)}, ");
         return m;
     }
 }
 
-Console.WriteLine(NaturalNumber(n, m));
+// 3. Метод всех методов и печати на экран.
+void MethodEveryoneMethods()
+{
+    int n = InputNaturalNumber(out n);
 
+    int m = 1;
+    Console.WriteLine(NaturalNumbersToBetweenRecursion(n, m));
+}
+
+MethodEveryoneMethods();
